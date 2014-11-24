fd193d1a3ac3b9959569bd4fa7de18e9b8d69c90
shecam13 <sheryl13c@gmail.com>
2014-11-24 17:00:52 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+	session_start();
+	//include 'php/header.php'; //contain all the codes that we will use in the webpage
+	//include 'php/footer.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Website UI</title>
+		<link rel = "stylesheet" href = "css/bootstrap.min.css">
+		<link rel = "stylesheet" href = "css/bootstrap.css" media= "screen">
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="page-header">
+				<div class="row">
+					<div class="col-lg-12">
+						REGISTER AND LOGIN
+					</div>
+				</div>
+
+				<div class="row">
+					<div class = "col-lg-12">
+						<div class = "navbar navbar-default">
+							 <div class="container-fluid">
+							    <!-- Brand and toggle get grouped for better mobile display -->
+							    <div class="navbar-header">
+							      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							        <span class="sr-only">Toggle navigation</span>
+							        <span class="icon-bar"></span>
+							        <span class="icon-bar"></span>
+							        <span class="icon-bar"></span>
+							      </button>
+							      <a class="navbar-brand" href="#">Home</a>
+							    </div>
+
+							    <!-- Collect the nav links, forms, and other content for toggling -->
+							    <div class="navbar-collapse collapse" >
+							      <ul class="nav navbar-nav">
+							        <li class="dropdown">
+							          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Desktops <b class="caret"></b></a>
+							          <ul class="dropdown-menu">
+							            <li><a href="#">Action</a></li>
+							            <li><a href="#">Another action</a></li>
+							            <li><a href="#">Something else here</a></li>
+							            <li class="divider"></li>
+							            <li><a href="#">Separated link</a></li>
+							            <li class="divider"></li>
+							            <li><a href="#">One more separated link</a></li>
+							          </ul>
+							        </li>
+							        <li class="dropdown">
+							          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Tablets<span class="caret"></span></a>
+							          <ul class="dropdown-menu" role="menu">
+							            
+							          </ul>
+							        </li>
+							        <li class="dropdown">
+							          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mobiles<span class="caret"></span></a>
+							          <ul class="dropdown-menu" role="menu">
+							            
+							          </ul>
+							        </li>
+							        <li class="dropdown">
+							          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Software<span class="caret"></span></a>
+							          <ul class="dropdown-menu" role="menu">
+							            
+							          </ul>
+							        </li>
+							        <li class="dropdown">
+							          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Audio<span class="caret"></span></a>
+							          <ul class="dropdown-menu" role="menu">
+							            
+							          </ul>
+							        </li>
+							      </ul>
+
+							      
+
+							      <ul class="nav navbar-nav navbar-right">
+							      	<form class="navbar-form navbar-left">
+								        <div class="form-group">
+								          <input type="text" class="form-control" placeholder="Search">
+								        </div>
+								        <button type="submit" class="btn btn-default">Submit</button>
+								     </form>
+								    <li><a href="#"><i class="glyphicon glyphicon-shopping-cart"></i></a></li>
+							        <li><a href="#">Link</a></li>
+
+
+							      </ul>
+							    </div><!-- /.navbar-collapse -->
+							</div><!-- /.container-fluid -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	
+	</body>
+	
+</html>
